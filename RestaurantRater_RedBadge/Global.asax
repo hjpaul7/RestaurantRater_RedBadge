153e051c27c5a7591fd36ffcc6f65d42067ba67e
Hustin Jeffers <hj.paul7@gmail.com>
2020-09-21 15:43:24 -0400

    Add project files.

---
+++ RestaurantRater_RedBadge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantRater_RedBadge.MvcApplication" Language="C#" %>
---
+++ RestaurantRater_RedBadge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater_RedBadge.Startup))]
+namespace RestaurantRater_RedBadge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
