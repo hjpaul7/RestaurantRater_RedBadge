dd628909d892f02bf54eb5470a3821b062ab932d
Hustin Jeffers <hj.paul7@gmail.com>
2020-09-22 14:10:44 -0400

    delete

--- RestaurantRater_RedBadge/Controllers/RestaurantController.cs
+++ RestaurantRater_RedBadge/Controllers/RestaurantController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RestaurantRater_RedBadge.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,56 @@ namespace RestaurantRater_RedBadge.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestauarantDbContext _db = new RestauarantDbContext();
         // GET: Restaurant
         public ActionResult Index()
         {
+            return View(_db.Restaurants.ToList());
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
